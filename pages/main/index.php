9f6a50e8c6e07bc0399317c1a0e1e6983e487e71
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-05 10:39:00 +0300

    add Banner

--- pages/main/index.php
+++ pages/main/index.php
@@ -6,33 +6,107 @@
         <div class="swiper-container banner__container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide banner__slide" style="background-image: url(/images/bg/bg-banner.jpg);">
-                    <div class="container">Slide 1</div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="banner__slide-left">
+                                <div class="banner__slide-content content">
+                                    <svg class="banner__slide-title" width="265" height="66"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 265 50">
+                                        <text x="0" y="40">Скидка</text>
+                                    </svg>
+                                    <div class="banner__slide-percent">
+                                        -25%
+                                    </div>
+                                    <div class="banner__slide-text">
+                                        на лампы filament
+                                    </div>
+                                </div>
+                                <a href="" class="button banner__slide-button">
+                                    <span class="button__text">Подробнее об акции</span>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide banner__slide" style="background-image: url(/images/bg/bg-banner.jpg);">
-                    <div class="container">Slide 2</div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="banner__slide-left">
+                                <div class="banner__slide-content content">
+                                    <svg class="banner__slide-title" width="265" height="66"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 265 50">
+                                        <text x="0" y="40">Скидка</text>
+                                    </svg>
+                                    <div class="banner__slide-percent">
+                                        -25%
+                                    </div>
+                                    <div class="banner__slide-text">
+                                        на лампы filament
+                                    </div>
+                                </div>
+                                <a href="" class="button banner__slide-button">
+                                    <span class="button__text">Подробнее об акции</span>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide banner__slide" style="background-image: url(/images/bg/bg-banner.jpg);">
-                    <div class="container">Slide 3</div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="banner__slide-left">
+                                <div class="banner__slide-content content">
+                                    <svg class="banner__slide-title" width="265" height="66"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 265 50">
+                                        <text x="0" y="40">Скидка</text>
+                                    </svg>
+                                    <div class="banner__slide-percent">
+                                        -25%
+                                    </div>
+                                    <div class="banner__slide-text">
+                                        на лампы filament
+                                    </div>
+                                </div>
+                                <a href="" class="button banner__slide-button">
+                                    <span class="button__text">Подробнее об акции</span>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
 
+        <div class="banner__nav">
+            <div class="container">
+                <div class="banner__nav-container">
 
-        <div class="container">
-            <div class="banner__nav">
+                    <!-- pagination -->
+                    <div class="swiper-pagination banner__pagination"></div>
+                    <!-- END pagination -->
 
-
-                <!-- pagination -->
-                <div class="swiper-pagination banner__pagination"></div>
-                <!-- END pagination -->
-
-                <!-- arrows -->
-                <div class="banner__arrows">
-                    <div class="banner__prev"></div>
-                    <div class="banner__next"></div>
+                    <!-- arrows -->
+                    <div class="banner__arrows">
+                        <div class="banner__prev banner__arrow">
+                            <svg class="banner__svg">
+                                <use xlink:href="images/icons/sprite.svg#left"></use>
+                            </svg>
+                        </div>
+                        <div class="banner__next banner__arrow">
+                            <svg class="banner__svg">
+                                <use xlink:href="images/icons/sprite.svg#right"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <!-- END arrows -->
                 </div>
-                <!-- END arrows -->
-
             </div>
         </div>
 
