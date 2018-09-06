7bbba729d7d1df9ff20a8895bab47d739459aba9
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-06 11:41:29 +0300

    add Actions on main tabs

--- pages/common/actions.php
+++ pages/common/actions.php
@@ -1,3 +1,103 @@
 <div class="actions">
+    <div class="row">
+        <!-- action -->
+        <a href="" title="Светодиодные лампы" class="action" style="background-image: url(images/content/actions/action1.jpg);">
 
+            <!-- img -->
+            <span class="action__img">
+                <span class="action__percent">
+                    Скидки -15%
+                </span>
+                <img src="images/content/actions/actionimg1.png" alt="Светодиодные лампы" title="Светодиодные лампы" class="action__pic">
+            </span>
+            <!-- END img -->
+
+            <!-- title -->
+            <span class="action__title">
+               Светодиодные лампы
+            </span>
+            <!-- END title -->
+        </a>
+        <!-- END action -->
+        <!-- action -->
+        <a href="" title="Энергосберегающие лампы" class="action action-2" style="background-image: url(images/content/actions/action2.jpg);">
+            <span class="action-2__title">
+               Энергосберегающие <br>
+                лампы
+            </span>
+            <span class="action-2__percent">
+                Скидка до 15%
+            </span>
+        </a>
+        <!-- END action -->
+        <!-- action -->
+        <a href="" title="Лампы накаливания" class="action action-3" style="background-image: url(images/content/actions/action3.jpg);">
+            <svg class="action-3__percent"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 300 90">
+                <text x="0" y="85">-20%</text>
+            </svg>
+            <span class="action-3__stick">
+                Спецпредложение на опт
+            </span>
+            <span class="action-3__title">
+               Лампы накаливания
+            </span>
+
+        </a>
+        <!-- END action -->
+        <!-- action -->
+        <a href="" title="Лампы люминесцентные" class="action action-4" style="background-image: url(images/content/actions/action4.jpg);">
+            <span class="action-4__stick">
+                Акция
+            </span>
+            <span class="action-4__title">
+               Лампы люминесцентные
+            </span>
+        </a>
+        <!-- END action -->
+        <!-- action -->
+        <a href="" title="Лампы галогенные" class="action action-5" style="background-image: url(images/content/actions/action5.jpg);">
+
+            <!-- img -->
+            <span class="action-5__img">
+                <span class="action-5__percent">
+                    -15%
+                </span>
+                <img src="images/content/actions/actionimg5.png" alt="Светодиодные лампы" title="Светодиодные лампы" class="action-5__pic">
+            </span>
+            <!-- END img -->
+
+            <span class="action-5__title">
+               Лампы галогенные
+            </span>
+        </a>
+        <!-- END action -->
+        <!-- action -->
+        <a href="" title="Прожекторы и фонари" class="action action-6" style="background-image: url(images/content/actions/action6.jpg);">
+            <span class="action-6__percent">
+                Акция -15%
+            </span>
+            <span class="action-6__title">
+               Прожекторы <br>
+                и фонари
+            </span>
+        </a>
+        <!-- END action -->
+    </div>
+    <!-- more -->
+    <div class="more">
+        <div class="row more__hidden">
+
+        </div>
+        <button role="button" class="more__button link">
+            <span class="link__text more__button-text">
+                Загрузить еще
+            </span>
+            <svg class="link__svg more__button-svg">
+                <use xlink:href="images/icons/sprite.svg#reload"></use>
+            </svg>
+        </button>
+    </div>
+    <!-- END more -->
 </div>
