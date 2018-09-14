016d6b10ef775883ffc2e015ed687db5da531e85
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-14 17:35:44 +0300

    add About 70%

---
+++ about.php
@@ -0,0 +1,9 @@
+<? include 'pages/top.php'?>
+
+<!-- main -->
+<div class="page">
+    <? include 'pages/about/index.php'?>
+</div>
+<!-- END main -->
+
+<? include 'pages/bottom.php'?>
---
+++ pages/about/index.php
@@ -0,0 +1,203 @@
+<main class="main">
+
+    <? include 'pages/common/banner3.php' ?>
+
+    <!-- category -->
+    <section class="section">
+        <div class="container">
+            <!-- top -->
+            <div class="section__top">
+
+                <!-- title -->
+                <div class="title">
+                    Корзина
+                </div>
+                <!-- END title -->
+
+            </div>
+            <!-- END top -->
+        </div>
+        <!-- about -->
+        <div class="about">
+
+            <div class="container">
+                <div class="row">
+                    <!-- left -->
+                    <div class="about__left">
+                        <div class="about__content">
+                            На данный момент мы представляем собой крупную компанию, владеющую интернет–магазином и
+                            имеющую в своей сети единый call-центр, который регулирует всю деятельность магазина, отдел
+                            продаж, службу доставки, широкий штат квалифицированных сборщиков, собственный склад c
+                            постоянным наличием необходимого запаса товаров.
+                        </div>
+                        <div class="about__desc">
+                            <div class="row">
+                                <div class="about__col">
+                                    За это время у нас сложились партнерские отношения
+                                    с ведущими производителями, позволяющие предлагать высококачественную продукцию по
+                                    конкурентоспособным ценам.
+                                </div>
+                                <div class="about__col">
+                                    Мы можем гордиться тем,
+                                    что у нас один из самых широких ассортиментов лампочек в городе и области.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END left -->
+
+                    <!-- right -->
+                    <div class="about__right">
+                        <div class="about__imgs">
+                            <div class="about__row">
+                                <div class="about__img">
+                                    <img src="images/content/about/about-1.jpg" alt="О компании" class="about__pic">
+                                </div>
+                                <div class="about__img about__img_1">
+                                    <img src="images/content/about/about-2.jpg" alt="О компании" class="about__pic">
+                                </div>
+                            </div>
+                            <div class="about__row">
+                                <div class="about__img about__img_2">
+                                    <img src="images/content/about/about-3.jpg" alt="О компании" class="about__pic">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END right -->
+                </div>
+            </div>
+
+            <!-- bg -->
+            <div class="about__bg">
+                <img src="/images/bg/bg-about.png" alt="О компании" class="about__bg-img">
+            </div>
+            <!-- END bg -->
+        </div>
+        <!-- END about -->
+
+        <!-- scope -->
+        <div class="scope">
+            <div class="container">
+                <div class="row">
+                    <!-- right -->
+                    <div class="scope__right">
+                        <div class="scope__title">
+                            Наши возможности
+                        </div>
+                        <div class="scope__content">
+                            Современные технологии стремительно проникают в нашу повседневную жизнь. Светодиодная
+                            продукция (LED) позволяет
+                            решать проблему не только качественного, но и эффективного света.
+                            Мы создали этот магазин, чтобы установка современного
+                            и качественного света в Вашем доме стала максимально простой
+                            и удобной для Вас и Вашей семьи.
+                        </div>
+
+                        <div class="scope__numbers">
+                            <div class="scope__number">
+                                <div class="scope__number-title">
+                                    135
+                                </div>
+                                <div class="scope__number-text">
+                                    Брендов
+                                </div>
+                            </div>
+                            <div class="scope__number">
+                                <div class="scope__number-title">
+                                    142
+                                </div>
+                                <div class="scope__number-text">
+                                    Сотрудника
+                                </div>
+                            </div>
+                            <div class="scope__number">
+                                <div class="scope__number-title">
+                                    11
+                                </div>
+                                <div class="scope__number-text">
+                                    лет на рынке
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- END right -->
+
+                    <!-- left -->
+                    <div class="scope__left">
+                        <div class="scope__list">
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                    <img src="/images/content/scope/scope1.png" alt="Быстрая доставка" title="Быстрая доставка" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Быстрая доставка
+                                </div>
+                            </div>
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                <img src="/images/content/scope/scope2.png" alt="Низкие цены" title="Низкие цены" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Низкие цены
+                                </div>
+
+                            </div>
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                <img src="/images/content/scope/scope3.png" alt="Широкий ассортимент" title="Широкий ассортимент" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Широкий ассортимент
+                                </div>
+
+                            </div>
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                    <img src="/images/content/scope/scope4.png" alt="Бонусы и подарки" title="Бонусы и подарки" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Бонусы и подарки
+                                </div>
+
+                            </div>
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                    <img src="/images/content/scope/scope5.png" alt="Надежные поставщики" title="Надежные поставщики" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Надежные поставщики
+                                </div>
+
+                            </div>
+                            <div class="scope__item">
+                                <div class="scope__img">
+                                    <img src="/images/content/scope/scope6.png" alt="Гарантия на все товары" title="Гарантия на все товары" class="scope__pic">
+                                </div>
+                                <div class="scope__text">
+                                    Гарантия на все товары
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END left -->
+
+
+                </div>
+            </div>
+        </div>
+        <!-- END scope -->
+
+        <!-- certificates -->
+        <div class="certs">
+
+        </div>
+        <!-- END certificates -->
+
+    </section>
+    <!-- END category -->
+
+
+</main>
