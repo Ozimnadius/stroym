e7df7d7a31590b9c2a53b1a65b24bca2eef4e077
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-17 13:27:08 +0300

    add Event

---
+++ pages/event/index.php
@@ -0,0 +1,74 @@
+<main class="main">
+
+    <? include 'pages/common/banner3.php' ?>
+
+    <section class="section">
+
+        <div class="container">
+            <!-- top -->
+            <div class="section__top">
+
+                <!-- title -->
+                <div class="title">
+                    Cвечки серии Ecola Light 5W и 6W
+                </div>
+                <!-- END title -->
+
+            </div>
+            <!-- END top -->
+
+            <!-- events -->
+            <div class="event">
+                <div class="row">
+                    <!-- left -->
+                    <div class="event__left">
+                        <img src="images/content/events/event1.jpg"
+                             title="Cвечки серии Ecola Light 5W и 6W"
+                             alt="Cвечки серии Ecola Light 5W и 6W"
+                             class="event__img">
+                        <div class="event__date">10 июля 2018</div>
+                    </div>
+                    <!-- END left -->
+
+                    <!-- right -->
+                    <div class="event__right">
+                        <div class="content">
+                            Появились новые свечки серии Ecola Light 5W и 6W. Как всегда, Ecola Light означает, что цена
+                            очень и очень привлекательная. Свечки матовые. Внешний вид очень симпатичный. А уж цены -
+                            так точно симпатичные: на 12-15% ниже, чем лампы основной линейки, и почти
+                            на 30% дешевле, чем серия Premium.
+
+                            <blockquote>
+                                Итак, 8 новых вариантов свечек: 5 и 6 ватт, 2 цветовые
+                                температуры (2700К и 4000К) и 2 варианта цоколя
+                                (Е14 и Е27) для каждой.
+                            </blockquote>
+
+                            Часть партнеров недооценивают свечи на цоколе Е27. А мы убедились, что покупатели удивляются
+                            и радуются, когда видят свечки на цоколь
+                            Е27. Так что свечки для цоколя Е27 пользуются стабильным спросом.
+
+                            <ul>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                                <li>C4TW50ELC Ecola Light candle LED 5,0W 220V E14 2700K свеча 100x37</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- END right -->
+                </div>
+            </div>
+            <!-- END events -->
+        </div>
+
+
+    </section>
+
+
+</main>
