4f876e3c63c28be4afb0a9059cadf925dabd56ab
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-17 12:15:26 +0300

    add Video

---
+++ pages/actions/index.php
@@ -0,0 +1,31 @@
+<main class="main">
+
+    <? include 'pages/common/banner3.php' ?>
+
+    <section class="section">
+
+        <div class="container">
+            <!-- top -->
+            <div class="section__top">
+
+                <!-- title -->
+                <div class="title">
+                    Акции
+                </div>
+                <!-- END title -->
+
+                <? include 'pages/common/sort.php' ?>
+
+            </div>
+            <!-- END top -->
+
+            <? include 'pages/common/actions.php' ?>
+        </div>
+
+
+
+
+    </section>
+
+
+</main>
---
+++ pages/videos/index.php
@@ -0,0 +1,128 @@
+<main class="main">
+
+    <? include 'pages/common/banner3.php' ?>
+
+    <section class="section">
+
+        <div class="container">
+            <!-- top -->
+            <div class="section__top">
+
+                <!-- title -->
+                <div class="title">
+                    Видео
+                </div>
+                <!-- END title -->
+
+                <? include 'pages/common/sort.php' ?>
+
+            </div>
+            <!-- END top -->
+
+            <!-- videos -->
+            <div class="videos">
+                <div class="videos__list">
+                    <div class="row">
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=NgI4c3MnjKA">
+                                    <img src="https://i.ytimg.com/vi/NgI4c3MnjKA/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                LED и галогеновые лампы H4
+                            </div>
+                        </div>
+                        <!-- END video -->
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=HSMv8tHd1ms">
+                                    <img src="https://i.ytimg.com/vi/HSMv8tHd1ms/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                Галогеновые лампы для дома
+                            </div>
+                        </div>
+                        <!-- END video -->
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=6sYKMKe5UPM">
+                                    <img src="https://i.ytimg.com/vi/6sYKMKe5UPM/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                Тест ламп H1 Выбираем самую яркую!
+                            </div>
+                        </div>
+                        <!-- END video -->
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=cVWRiIz3Nz0">
+                                    <img src="https://i.ytimg.com/vi/cVWRiIz3Nz0/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                Сравнение ламп накаливания, <br>
+                                галогеновых и энергосберегающих
+                            </div>
+                        </div>
+                        <!-- END video -->
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=_888T9wZwk4">
+                                    <img src="https://i.ytimg.com/vi/_888T9wZwk4/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                Как делают галогенки?
+                            </div>
+                        </div>
+                        <!-- END video -->
+                        <!-- video -->
+                        <div class="video">
+                            <div class="video__wrapper">
+                                <div class="proportion proportion_16x9"></div>
+                                <a class="video__fancy" data-fancybox href="https://www.youtube.com/watch?v=OdizNqBfZN0">
+                                    <img src="https://i.ytimg.com/vi/OdizNqBfZN0/maxresdefault.jpg?" alt=""
+                                         class="video__img">
+                                </a>
+                            </div>
+                            <div class="video__title">
+                                Как и к чему подключить <br>
+                                светодиодную ленту ?
+                            </div>
+                        </div>
+                        <!-- END video -->
+
+                    </div>
+                </div>
+                <div class="videos__pagination">
+                    <? include 'pages/common/pagination.php';?>
+                </div>
+            </div>
+            <!-- END videos -->
+        </div>
+
+
+
+
+    </section>
+
+
+</main>
