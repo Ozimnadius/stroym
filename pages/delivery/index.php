73e3ab261f129187e0479e0331fc1529bc9ccf95
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-17 17:16:13 +0300

    add Delivery

---
+++ pages/delivery/index.php
@@ -0,0 +1,203 @@
+<main class="main">
+
+    <? include 'pages/common/banner3.php' ?>
+
+    <section class="section">
+        <div class="container">
+
+            <!-- top -->
+            <div class="section__top">
+
+                <!-- title -->
+                <div class="title">
+                    Доставка
+                </div>
+                <!-- END title -->
+
+            </div>
+            <!-- END top -->
+
+            <!-- delivery -->
+            <div class="delivery">
+                <div class="container">
+
+                    <!-- top -->
+                    <div class="delivery__top">
+                        <div class="row">
+                            <div class="delivery__content">
+                                <div class="content">
+                                    Доставка осуществляется по адресу, указанному при оформлении заказа. Если необходимо
+                                    доставить товар по иному адресу, необходимо сообщить адрес менеджеру Службы
+                                    доставки, который свяжется с вами непосредственно после оформления заказа на сайте
+                                    <blockquote style="font-size: 20px;">
+                                        При отказе от всего доставленного товара, покупатель оплачивает стоимость
+                                        доставки в размере 250 руб.
+                                        + расходы за МКАД
+                                    </blockquote>
+                                </div>
+
+                                <ul class="delivery__ul">
+                                    <li class="delivery__li">
+                                        <svg class="delivery__li-svg">
+                                            <use xlink:href="images/icons/sprite.svg#delivery"></use>
+                                        </svg>
+                                        <div class="delivery__li-text">
+                                            Возможна доставка в <b>любой город РФ</b>
+                                        </div>
+                                    </li>
+                                    <li class="delivery__li">
+                                        <svg class="delivery__li-svg">
+                                            <use xlink:href="images/icons/sprite.svg#sroki"></use>
+                                        </svg>
+                                        <div class="delivery__li-text">
+                                            Срок доставки: <b>1-3 дня</b>
+                                        </div>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- END top -->
+
+                    <!-- middle -->
+                    <div class="delivery__middle">
+                        <div class="delivery__list">
+                            <div class="row">
+                                <div class="delivery__item">
+                                    <!-- icon -->
+                                    <div class="delivery__icon">
+                                        <svg class="delivery__icon-svg">
+                                            <use xlink:href="images/icons/sprite.svg#kreml"></use>
+                                        </svg>
+                                    </div>
+                                    <!-- END icon -->
+
+                                    <!-- title -->
+                                    <div class="delivery__title">
+                                        Москва и Московская область
+                                    </div>
+                                    <!-- END title -->
+
+                                    <div class="delivery__desc">
+                                        <div class="delivery__text">
+                                            При заказе на сумму до 5 000 рублей доставка: <br>
+                                            <b>250 руб + 30 руб./км от МКАД</b>
+                                        </div>
+                                        <div class="delivery__text">
+                                            При заказе от 5 000 рублей доставка: <br>
+                                            <b>30 руб./км от МКАД</b>
+                                        </div>
+                                        <div class="delivery__text">
+                                            Срок доставки: <br>
+                                            <b>1-2 рабочих дня, по договоренности</b>
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+                                <div class="delivery__item">
+                                    <!-- icon -->
+                                    <div class="delivery__icon">
+                                        <svg class="delivery__icon-svg">
+                                            <use xlink:href="images/icons/sprite.svg#russia"></use>
+                                        </svg>
+                                    </div>
+                                    <!-- END icon -->
+
+                                    <!-- title -->
+                                    <div class="delivery__title">
+                                        По всей России
+                                    </div>
+                                    <!-- END title -->
+
+                                    <div class="delivery__desc">
+                                        <div class="delivery__text">
+                                            Отправка заказа через транспортные компании <br>
+                                            <b>осуществляется при 100% оплате товара</b>
+                                        </div>
+                                        <div class="delivery__text">
+                                            Услуги ТК оплачиваются отдельно <br>
+                                            <b>при получении товара</b>
+                                        </div>
+                                        <div class="delivery__text">
+                                            Сроки доставки транспортных компаний составляют: <br>
+                                            <b>3-10 дней со дня поступления оплаты</b>
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END middle -->
+
+                    <!-- bottom -->
+                    <div class="delivery__bottom">
+                        <img src="/images/content/delivery.png" alt="Доставка" title="Доставка" class="delivery__img">
+                        <div class="delivery__minimal">
+                            <svg class="delivery__minimal-svg">
+                                <use xlink:href="images/icons/sprite.svg#error"></use>
+                            </svg>
+                            <div class="delivery__minimal-text">
+                                Уведомляем, что минимальная сумма заказа на нашем сайте составляет 3 000 рублей
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END bottom -->
+                </div>
+            </div>
+            <!-- END delivery -->
+
+        </div>
+    </section>
+
+    <!-- conditions -->
+    <div class="conditions">
+        <div class="container">
+            <div class="row">
+                <div class="conditions__left">
+                    <div class="conditions__title">
+                        Условия оплаты товара
+                    </div>
+                    <div class="conditions__text">
+                        Как только заказ прибудет в ваш город, с вами свяжется сотрудник транспортной компании. Перед
+                        получением покупки вам необходимо будет оплатить стоимость доставки и, если вы не совершали
+                        предоплаты, стоимость заказа. Возврат заказных товаров возможен только в случае обнаружения
+                        производственного брака. Обязательно проверьте товар при получении!
+                    </div>
+                </div>
+                <div class="conditions__right">
+                    <div class="conditions__list">
+                        <div class="condition">
+                            <svg class="condition__svg">
+                                <use xlink:href="images/icons/sprite.svg#card"></use>
+                            </svg>
+                            <div class="condition__text">
+                                Оплата за безналичный расчет
+                                для юридических лиц
+                            </div>
+                        </div>
+                        <div class="condition">
+                            <svg class="condition__svg">
+                                <use xlink:href="images/icons/sprite.svg#expeditor"></use>
+                            </svg>
+                            <div class="condition__text">
+                                Оплата за безналичный расчет
+                                для юридических лиц
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END conditions -->
+
+    <? include 'pages/common/look2.php'?>
+</main>
